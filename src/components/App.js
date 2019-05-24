e45638ea5d0e10b38bd807c571bb2faa20fe0978
Emre Akurek <emreyeprem@gmail.com>
2019-05-24 12:49:39 -0500

    actionTypes success

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,19 @@ import React, {Component} from 'react';
 import './App.css';
 
 function* testing(){
-  yield 1;
-  yield 2;
-  yield 3;
+  while (true){
+    yield 1;
+    yield 2;
+    yield 3;
+  }
 }
 
 class App extends Component {
   render() {
     const iterator = testing();
+    console.log(iterator.next());
+    console.log(iterator.next());
+    console.log(iterator.next());
     console.log(iterator.next());
   return (
      <div>
